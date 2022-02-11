5dc9175449c798c50f2df4d4c6c37d14ed7ede48
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-02-11 19:25:36 +0300

    Решено 4 задачи по теме: функции

---
+++ Функции/Isinstance/Проверить является ли объект экземпляром указанного класса.py
@@ -0,0 +1,19 @@
+"""
+Требуется проверить является ли объект экземпляром указанного класса
+
+Sample Input 1:
+1, int
+
+Sample Output 1:
+True
+
+Sample Input 2:
+'text', str
+
+Sample Output 2:
+True
+
+"""
+
+n = input()
+print(isinstance(n, str))
---
+++ Функции/Issubclass/Проверить является ли указанный класс подклассом указанного класса.py
@@ -0,0 +1,20 @@
+"""
+Требуется проверить является ли указанный класс подклассом указанного класса.
+
+Класс считается подклассом самого себя.
+
+Sample Input 1:
+int, int
+
+Sample Output 1:
+True
+
+Sample Input 2:
+int, str
+
+Sample Output 2:
+False
+
+"""
+
+print(issubclass(str, str))
