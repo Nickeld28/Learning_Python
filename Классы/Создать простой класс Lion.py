7fe29dedc887fef2b3891fd95cdc1caf5f03ce83
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-02-14 21:35:25 +0300

    Решено 2 задачи по теме ООП и добавлен конспект к лекции

---
+++ Классы/Создать простой класс Lion.py
@@ -0,0 +1,14 @@
+"""
+Создайте класс Lion. В нем должен быть метод roar, который печатает на экран "Rrrrrrr!!!"
+
+Пример работы с классом Lion
+
+simba = Lion()
+simba.roar() # печатает Rrrrrrr!!!
+"""
+
+
+class Lion:
+    @staticmethod
+    def roar():
+        print("Rrrrrrr!!!")
